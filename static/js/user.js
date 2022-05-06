10b021a968246eb637cf721370ae2e32dab27f82
DongwooKim <kimphysicsman@gmail.com>
2022-05-06 10:24:32 +0900

    추가 - 유저페이지 프로필 부분 반응형

--- static/js/user.js
+++ static/js/user.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
 // 메뉴 클릭시 클릭한 메뉴 활성화
 // name : 클릭한 메뉴 태그의 name
 function user_menu_on(name){
-    $('.user_posts_menu').css('border-top', '1px solid #e8e8e8');
-    $('.user_posts_menu').css('font-weight', '300');
-    $('.user_posts_menu').css('opacity', '0.5');
+    $('.user_post_menu').css('border-top', '1px solid #e8e8e8');
+    $('.user_post_menu').css('font-weight', '300');
+    $('.user_post_menu').css('opacity', '0.5');
 
-    $(`.user_posts_menu[name=${name}]`).css('border-top', '2px solid black');
-    $(`.user_posts_menu[name=${name}]`).css('font-weight', 'bold');
-    $(`.user_posts_menu[name=${name}]`).css('opacity', '1');
+    $(`.user_post_menu[name=${name}]`).css('border-top', '2px solid black');
+    $(`.user_post_menu[name=${name}]`).css('font-weight', 'bold');
+    $(`.user_post_menu[name=${name}]`).css('opacity', '1');
 }
