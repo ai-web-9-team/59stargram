6e8a21deb53b68b608093da8f78f9826399574a8
DongwooKim <kimphysicsman@gmail.com>
2022-05-07 18:34:22 +0900

    추가 - 유저페이지 팔로우 모달창

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ def home():
 def user():
     return render_template('user.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- static/js/user.js
+++ static/js/user.js
@@ -14,3 +14,48 @@ function user_menu_on(name){
     $(`.user_post_menu[name=${name}]`).css('font-weight', 'bold');
     $(`.user_post_menu[name=${name}]`).css('opacity', '1');
 }
+
+const body = document.querySelector('body');
+const modal_outside = document.querySelector('.user_follower_box');
+
+
+// 팔로우 모달 API 전달하기
+// type - 0:팔로워, 1:팔로잉
+function user_modal_on(type) {
+
+    console.log(type);
+
+    let title_text;
+    if (type == 0) {
+        title_text = '팔로워';
+    } else if (type == 1) {
+        title_text = '팔로잉';
+    } else {
+        return;
+    }
+
+    $('.user_follower_title_text').text(title_text);
+
+    modal_outside.classList.toggle('show');
+    if (modal_outside.classList.contains('show')) {
+        body.style.overflow = 'hidden';
+    }
+}
+
+
+
+// 모달 밖을 클릭하면 모달창 사라지기
+function user_modal_quit() {
+    modal_outside.classList.toggle('show');
+
+  if (!modal_outside.classList.contains('show')) {
+    body.style.overflow = 'auto';
+  }
+}
+
+
+modal_outside.addEventListener('click', (event) => {
+if (event.target === modal_outside) {
+    user_modal_quit()
+}
+});
