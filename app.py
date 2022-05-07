11bd4491abc731e55dd6f1582caa49aebff07e20
kimheejeong3808 <khjhj3808@gmail.com>
2022-05-07 16:24:32 +0900

    detail.html 파일 templates에 넣고, css분리 detail.js생성 (스테이지에 올리라는게 너무 많음...??!!  뭔지 모르겠는데 일단 커밋할게요ㅠㅠ)

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/user')
+def home():
+    return render_template('user.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
